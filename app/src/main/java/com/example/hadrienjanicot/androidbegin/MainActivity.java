4700da56b3bafea5cd357a0f38f47208e5a4dccc
Hadrien Janicot <hadrien.janicot@etu.emse.fr>
2018-10-22 17:58:26 +0200

    Utilisation des fragments

--- app/src/main/java/com/example/hadrienjanicot/androidbegin/MainActivity.java
+++ app/src/main/java/com/example/hadrienjanicot/androidbegin/MainActivity.java
@@ -2,32 +2,35 @@ package com.example.hadrienjanicot.androidbegin;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements CreateFragment.OnFragmentInteractionListener{
     static Cellar cave = new Cellar();
-    IconDataAdapter adapter = new IconDataAdapter(cave.bottleList);
+    Button addBottleButton;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button addBottleButton= findViewById(R.id.a_main_btn_addB);
-        //listDisplay = findViewById(R.id.a_main_bottleList);
+        addBottleButton= findViewById(R.id.a_main_btn_addB);
+
+        fragmentDisplayCreation();
+
         addBottleButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CreateBottleActivity.class);
-                startActivityForResult(intent,0);
+                //Intent intent = new Intent(MainActivity.this, CreateBottleActivity.class);
+                //startActivityForResult(intent,0);
+                addBottleButton.setVisibility(View.GONE);
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                CreateFragment fragment = new CreateFragment();
+                fragmentTransaction.replace(R.id.container, fragment);
+                fragmentTransaction.commit();
             }
         });
-        RecyclerView recycler = findViewById(R.id.a_main_recycler);
-        recycler.setAdapter(adapter);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        recycler.setLayoutManager(layoutManager);
     }
 
     @Override
@@ -37,8 +40,27 @@ public class MainActivity extends AppCompatActivity {
             Bottle createdBottle ;
             createdBottle = (Bottle) data.getSerializableExtra("bottle");
             cave.addBottle(createdBottle);
-            adapter.notifyItemInserted(cave.getNumberOfBottles());
-            //listDisplay.setText(cave.cellarView());
+            //adapter.notifyItemInserted(cave.getNumberOfBottles());
+            fragmentDisplayCreation();
+
         }
+    }
+
+    protected void fragmentDisplayCreation(){
+        addBottleButton.setVisibility(View.VISIBLE);
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("bottleList", cave.bottleList);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        DisplayFragment fragment = new DisplayFragment();
+        fragment.setArguments(bundle);
+        fragmentTransaction.replace(R.id.container, fragment);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void AddBottle(Bottle b) {
+        cave.addBottle(b);
+        fragmentDisplayCreation();
     }
 }
